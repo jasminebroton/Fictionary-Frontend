5f2832d40a532fc05ce49d257421896211376d9b
Jasmine Broton <jbroton@emich.edu>
2024-02-04 12:29:48 -0500

    [page] voting
    issue #12

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Room from "./Room";
+import Voting from "./Voting"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/room/:roomId" element={<Room />} />
+          <Route path="/voting" element= {<Voting />} />
           {/* ellen: setup route by adding path (url) and element (react component) */}
           {/* example: <Route path="my/URL" element={<MyComponent />} */}
         </Routes>

448913c9b6fc36324f3a2ce688681a0bfc589602
Hunter Cox <hcox7@emich.edu>
2024-02-11 18:24:53 -0500

    Feat: added rules, updated home and added styles.
    This comit added the Rules for the Rules page with buttons to scroll the rules,
    added user styles for tailwind, and added button for the home page.
    
    * Feat: Added rules for the page,
    * Added buttons to scroll the rules.
    * fix: Changed the div buttons for actual buttons.
    * added text and button styles for reference if needed for UI cohesion
    
    addresses #18

--- src/Home.js
+++ src/Home.js
@@ -21,11 +21,17 @@ function Home() {
     }
     
     return (
-        <div className="bg-[#ece6c2] font-serif h-screen">
-            <div className="text-[#6f5643] text-5xl pt-10 mb-32">Fictionary</div>
-            <div className="bg-[#cc6b49] text-[#ece6c2] w-fit p-2 mx-auto" onClick={sendToHost} >Create a Room</div>
-            <div className="bg-[#cc6b49] text-[#ece6c2] w-fit p-4 mx-auto" onClick={sendToGuest} >Join a Game</div>
-            <div className="bg-[#cc6b49] text-[#ece6c2] w-fit p-6 mx-auto" onClick={sendToRules} >Rules</div>
+        <div className="bg-[#ece6c2] font-serif h-screen space-y-3">
+            <div className="text-[#6f5643] text-5xl pt-10 mb-32 ">Fictionary</div>
+            <div>
+                <button className="fic-button w-fit p-4 mx-auto" onClick={sendToHost} >Create a Room</button>
+            </div>
+            <div>
+                <button className="fic-button w-fit p-4 mx-auto" onClick={sendToGuest} >Join a Game</button>
+            </div>
+            <div>
+                <button className="fic-button w-fit p-4 mx-auto" onClick={sendToRules} >Rules</button>
+            </div>
         </div>
     )
 }

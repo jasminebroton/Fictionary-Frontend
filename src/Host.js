1de7a33df9db93ef181420116b8ef06296de32e1
Jasmine Broton <jbroton@emich.edu>
2024-04-08 11:39:07 -0400

    remote

--- src/Host.js
+++ src/Host.js
@@ -1,6 +1,8 @@
 import { useParams } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 
+const EXPRESS_SERVER_URL = process.env.REACT_APP_SOCKET_SERVER_URL;
+
 function Host() {
     const {roomId} = useParams();
     const navigate = useNavigate();
@@ -9,6 +11,7 @@ function Host() {
     {   
         const guestName = document.getElementById("name").value;
         if(guestName !== "") {
+            sendRoomId(roomId);
             navigate(`/room/${roomId}`, {replace: false, state: {"host": true, "name": guestName}});
             // see issue #16
             // window.location.reload();
@@ -18,7 +21,27 @@ function Host() {
     function returnHome(){
         navigate('/');
     }
-
+    
+    const sendRoomId = async (roomId) => {
+        try {
+          const response = await fetch(`${EXPRESS_SERVER_URL}/addRoomId`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ roomId }),
+          });
+      
+          if (!response.ok) {
+            throw new Error('Failed to send roomId to backend');
+          }
+      
+          console.log('RoomId sent to backend successfully');
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      };
+      
     return (
         <div className="background custom-text flex flex-col space-y-12">
             <div className="header mb-20">Fictionary</div>
